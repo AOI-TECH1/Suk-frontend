bed8610aafa7f493d314ecfbe9b038d415a2e6a2
Oshinkoya Masturah <oshinkoyamasturah@gmail.com>
2026-05-09 16:51:22 +0100

    fixes

--- src/components/public/homepage/TrendingSlide.jsx
+++ src/components/public/homepage/TrendingSlide.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import slide1 from "../../assets/images/cat-back.jpg";
+import slide1 from "../../../assets/images/cat-back.jpg";
 
 function TrendySlide() {
   const [index, setIndex] = useState(0);
